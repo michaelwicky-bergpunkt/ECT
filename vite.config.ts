bca656ed32872ebaf65b2127c2b57d3d1147c24b
michaelwicky-bergpunkt <michael.wicky@bergpunkt.ch>
2026-03-19 14:26:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,17 +26,17 @@ export default defineConfig(({ mode }) => {
           scope: '/ECT/',
           icons: [
             {
-              src: '/ECT/pwa-192x192.png',
+              src: 'pwa-192x192.png',
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: '/ECT/pwa-512x512.png',
+              src: 'pwa-512x512.png',
               sizes: '512x512',
               type: 'image/png',
             },
             {
-              src: '/ECT/pwa-512x512.png',
+              src: 'pwa-512x512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable',
